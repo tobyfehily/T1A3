42638bd3495ff8c3a5ea2f3ce0afd001c0d026de
tobyfehily <tobyfehily@gmail.com>
2023-10-23 10:07:54 +1100

    Prevent user from entering alreadyt added books

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
+# import gc
+
 book_list = []
 
-def add_book(title, author):
-    new_book = dict(title = title, author = author)
-    book_list.append(new_book)
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
 
-title = input("Enter book title: ")
-author = input("Enter author: ")
-add_book(title, author)
+    def __eq__(self, other):
+        if isinstance(other, Book):
+            return self.title == other.title and self.author == other.author
+        return False
+
+# def add_book(title, author):
+#     new_book = dict(title = title, author = author)
+#     book_list.append(new_book)
+
+book1 = Book("title", "author")
+book_list.append(book1)
+
+title = input("Enter book title: ").lower()
+author = input("Enter author: ").lower()
+new_book = Book(title, author)
+if new_book in book_list:
+    del new_book
+    print("We got it!")
+
+# for ob in gc.get_objects():
+#     if isinstance(ob, Book):
+#         print(ob.title, ob.author)
