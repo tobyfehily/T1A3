7ceaa1ffe3f6e8187d783e37c3f699a837b8bf0a
tobyfehily <tobyfehily@gmail.com>
2023-10-23 12:56:57 +1100

    Update books by percentage

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import random
 
 book_list = [
-    {'title': 'title', 'author': 'author', 'pages': 200, 'currently_reading': False, 'progress': 0},
-    {'title': 'title2', 'author': 'author2', 'pages': 300, 'currently_reading': False, 'progress': 0}, 
-    {'title': 'title3', 'author': 'author3', 'pages': 500, 'currently_reading': False, 'progress': 0},
+    {'title': 'title', 'author': 'author', 'pages': 200, 'currently_reading': False, 'pages_read': 0},
+    {'title': 'title2', 'author': 'author2', 'pages': 300, 'currently_reading': False, 'pages_read': 0}, 
+    {'title': 'title3', 'author': 'author3', 'pages': 500, 'currently_reading': False, 'pages_read': 0},
     ]
 
-def add_book(new_title, new_author, new_pages, currently_reading = False, progress = 0):
-    new_book = dict(title = new_title, author = new_author, pages = new_pages, currently_reading = currently_reading, progress = progress)
+def add_book(new_title, new_author, new_pages, currently_reading = False, pages_read = 0):
+    new_book = dict(title = new_title, author = new_author, pages = new_pages, currently_reading = currently_reading, pages_read = pages_read)
     book_list.append(new_book)
     print(f"{new_title} by {new_author} has been added.")
 
@@ -22,7 +22,9 @@ def get_book_list():
         print(f"{i + 1}: '{book['title']}' by {book['author']}")
 
 def delete_book(index):
+    print(f"{book_list[index - 1]['title']} by {book_list[index - 1]['author']} has been deleted.")
     del book_list[index - 1]
+
 
 def set_current_book(index):
     book_list[index -1]["currently_reading"] = True
@@ -32,13 +34,21 @@ def get_random_book():
     random_book = random.choice(book_list)
     print(f"Why not try {random_book['title']} by {random_book['author']}?")
 
-def set_book_progress(index, new_progress):
-    if new_progress > book_list[index - 1]["pages"]:
+def set_book_pages(index, new_pages):
+    if new_pages > book_list[index - 1]["pages"]:
         print(f"{book_list[index - 1]['title']} by {book_list[index - 1]['author']} is only {book_list[index - 1]['pages']} pages long.")
+    elif new_pages == book_list[index - 1]["pages"]:
+        print("You're all finished!")
+        delete_book(index)
     else:
-        book_list[index - 1]['progress'] = new_progress
-        print(f"Only {book_list[index - 1]['pages'] - book_list[index - 1]['progress']} pages to go!")
+        book_list[index - 1]['pages_read'] = new_pages
+        print(f"Only {book_list[index - 1]['pages'] - book_list[index - 1]['pages_read']} pages to go!")
 
+def set_book_percent(index, new_percent):
+    while new_percent < 0 or new_percent > 100:
+        new_percent = input("Please enter a valid percentage between 0 and 100.")
+    else:
+        book_list[index - 1]["pages_read"] = round((float(new_percent) * float(book_list[index - 1]["pages"])) / 100) 
 
 # while True:
 #     new_title = input("Enter book title: ").lower()
@@ -60,6 +70,8 @@ def set_book_progress(index, new_progress):
 #             continue
 
 
+
+
 get_book_list()
-set_book_progress(2, 250)
+set_book_percent(2, 89)
 print(book_list)
