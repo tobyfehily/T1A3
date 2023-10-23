bd7f777047d267f7d72430946f3bf06b3cb4d24d
tobyfehily <tobyfehily@gmail.com>
2023-10-23 12:02:16 +1100

    Fixed adding multiple books

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ book_list = [
     {'title': 'title3', 'author': 'author3'}
     ]
 
-
-def add_book(title, author):
-    new_book = dict(title = title, author = author)
+def add_book(new_title, new_author):
+    new_book = dict(title = new_title, author = new_author)
     book_list.append(new_book)
+    print(f"{new_title} by {new_author} has been added.")
+
+def check_book_dupes(new_title, new_author):
+    for book in book_list:
+        if book['title'] == new_title and book['author'] == new_author:
+            return True
+    return False
 
 def get_book_list():
     for i, book in enumerate(book_list):
@@ -22,16 +28,19 @@ def get_random_book():
     random_book = random.choice(book_list)
     print(f"Why not try {random_book['title']} by {random_book['author']}?")
 
+
+
+
 while True:
     new_title = input("Enter book title: ").lower()
     new_author = input("Enter author: ").lower()
-    for book in book_list:
-        if book['title'] == new_title and book['author'] == new_author:
-            print(f"You have already added '{new_title}' by {new_author}.")
-            break
-    add_book(new_title, new_author)
+    if check_book_dupes(new_title, new_author):
+        print(f"You have already added '{new_title}' by {new_author}.")
+        continue
+    else:
+        add_book(new_title, new_author)
 
-    continue_prompt = input(f"{new_title} by {new_author} has been added. Would you like to add another book (y/n)?: ").lower()
+    continue_prompt = input("Would you like to add another book (y/n)?: ").lower()
     while continue_prompt not in ["y", "n"]:
         continue_prompt = input("Please enter 'y' to continue adding books or 'n' to stop: ").lower()
     else:
