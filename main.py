f0e815e6398d4ab587baca4ee663d64c00252605
tobyfehily <tobyfehily@gmail.com>
2023-10-23 12:38:06 +1100

    Update book progress by pages

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import random
 
 book_list = [
-    {'title': 'title', 'author': 'author', 'pages': 200, 'currently_reading': False},
-    {'title': 'title2', 'author': 'author2', 'pages': 300, 'currently_reading': False}, 
-    {'title': 'title3', 'author': 'author3', 'pages': 500, 'currently_reading': False}
+    {'title': 'title', 'author': 'author', 'pages': 200, 'currently_reading': False, 'progress': 0},
+    {'title': 'title2', 'author': 'author2', 'pages': 300, 'currently_reading': False, 'progress': 0}, 
+    {'title': 'title3', 'author': 'author3', 'pages': 500, 'currently_reading': False, 'progress': 0},
     ]
 
-def add_book(new_title, new_author, new_pages, currently_reading = False):
-    new_book = dict(title = new_title, author = new_author, pages = new_pages, currently_reading = currently_reading)
+def add_book(new_title, new_author, new_pages, currently_reading = False, progress = 0):
+    new_book = dict(title = new_title, author = new_author, pages = new_pages, currently_reading = currently_reading, progress = progress)
     book_list.append(new_book)
     print(f"{new_title} by {new_author} has been added.")
 
@@ -21,7 +21,7 @@ def get_book_list():
     for i, book in enumerate(book_list):
         print(f"{i + 1}: '{book['title']}' by {book['author']}")
 
-def remove_book(index):
+def delete_book(index):
     del book_list[index - 1]
 
 def set_current_book(index):
@@ -32,7 +32,12 @@ def get_random_book():
     random_book = random.choice(book_list)
     print(f"Why not try {random_book['title']} by {random_book['author']}?")
 
-
+def set_book_progress(index, new_progress):
+    if new_progress > book_list[index - 1]["pages"]:
+        print(f"{book_list[index - 1]['title']} by {book_list[index - 1]['author']} is only {book_list[index - 1]['pages']} pages long.")
+    else:
+        book_list[index - 1]['progress'] = new_progress
+        print(f"Only {book_list[index - 1]['pages'] - book_list[index - 1]['progress']} pages to go!")
 
 
 # while True:
@@ -56,5 +61,5 @@ def get_random_book():
 
 
 get_book_list()
-set_current_book(2)
+set_book_progress(2, 250)
 print(book_list)
